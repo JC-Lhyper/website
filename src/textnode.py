579a1b8ae9b6e526b24878bf8fd7a18cb9fa9bfd
JC-Lhyper <lippert.jc@gmail.com>
2024-09-27 10:48:44 +0200

    Création des scripts pour lancer main.py depuis bash et création d'un TextNode d'exemple

---
+++ src/textnode.py
@@ -0,0 +1,14 @@
+
+class TextNode:
+    def __init__(self, text, text_type, url = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if not isinstance(other, TextNode):  # Vérifie si other est une instance de TextNode
+            raise TypeError(f"Comparaison impossible : {other} n'est pas un objet de type TextNode.")
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
